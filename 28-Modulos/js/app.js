e518c19744aef118b6731648ca2f97df1fe658c5
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-09-23 11:24:55 -0600

    Modulos en JS

---
+++ 28-Modulos/js/app.js
@@ -0,0 +1,19 @@
+import nuevaFuncion, { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from "./cliente.js";
+import { Empresa } from "./empresa.js";
+
+nuevaFuncion();
+
+console.log( nombreCliente );
+console.log( ahorro );
+
+console.log( mostrarInformacion(nombreCliente, ahorro) );
+
+tieneSaldo(ahorro);
+
+const cliente = new Cliente( nombreCliente, ahorro );
+
+console.log(cliente.mostrarInformacion());
+
+// Importar empresa
+const empresa = new Empresa('Código con Juan', 100, 'Aprendizaje en Línea');
+console.log(empresa.mostrarInformacion());
---
+++ 28-Modulos/js/cliente.js
@@ -0,0 +1,37 @@
+// Basicos de los modulos en ES6
+// (function() {
+//     console.log('Desde un IIFE');
+
+//     // const nombreCliente = 'Juan';
+//     window.nombreCliente = 'Juan';
+// })();
+
+export const nombreCliente = 'Juan';
+export const ahorro = 200;
+
+export function mostrarInformacion(nombre, ahorro) {
+    return `Cliente: ${nombre} - Ahorro: ${ahorro}`;
+}
+
+export function tieneSaldo(ahorro) {
+    if (ahorro > 0) {
+        console.log('Si tiene saldo');
+    } else {
+        console.log('El Cliente no tiene saldo');
+    }
+}
+
+export class Cliente {
+    constructor(nombre, ahorro) {
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+    }
+
+    mostrarInformacion() {
+        return `Cliente: ${this.nombre} - Ahorro: ${this.ahorro}`;
+    }
+}
+
+export default function nuevaFuncion() {
+    console.log('Este es el export default');
+}
