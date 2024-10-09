62bf46c540c1671a2b4d9a06d1b42af5fbdb5f73
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-10-09 16:53:00 -0600

    API's en JS, Notificacion API, Interseection Observer, Speech API

---
+++ 34-APIs-JS/js/02-app.js
@@ -0,0 +1,16 @@
+// Previamente vimos getclientRect, que nos permitia identificar cuando un elemento estaba visible
+// existe otra API llamada Intersection Ovserver, veamos como utilizarla.
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const observer = new IntersectionObserver( entries => {
+
+        if (entries[0].isIntersecting) {
+            console.log('Ya esta visible');
+        }
+    });
+
+    observer.observe(document.querySelector('.premium'));
+
+
+})
