e739776ba25b9d9814917b7cb9d55a9bed081ca4
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-11-25 17:57:23 -0600

    Actualizar la cantidad si un producto es duplicado

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -4,6 +4,12 @@ let cliente = {
     pedido: []
 };
 
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
@@ -69,15 +75,72 @@ function mostrarPlatillos(platillos) {
 
     platillos.forEach( platillo => {
         const row = document.createElement('DIV');
-        row.classList.add('row');
+        row.classList.add('row', 'py-3', 'border-top');
 
         const nombre = document.createElement('DIV');
         nombre.classList.add('col-md-4');
         nombre.textContent = platillo.nombre;
 
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3', 'fw-bold');
+        precio.textContent = `$${platillo.precio}`;
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3');
+        categoria.textContent = categorias[ platillo.categoria ];
+
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${platillo.id}`;
+        inputCantidad.classList.add('form.control');
+
+        // Función que detecta la cantidad y el platillo que se esta agregando
+        inputCantidad.onchange = function() {
+            const cantidad = parseInt( inputCantidad.value );
+            agregarPlatillo({...platillo, cantidad});
+        }
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
+
         row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+        row.appendChild(agregar);
 
         contenido.appendChild(row);
     })
 }
 
+function agregarPlatillo(producto) {
+    // Extraer el pedido actual
+    let { pedido } = cliente;
+
+    // Revisar que la cantidad sea mayor a 0
+    if (producto.cantidad > 0) {
+
+        // Comprueba si el elemento ya existe en el array
+        if( pedido.some( articulo => articulo.id === producto.id ) ){
+            // El articulo ya existe, Actualizar la cantidad
+            const pedidoActualizado = pedido.map( articulo => {
+                if (articulo.id === producto.id) {
+                    articulo.cantidad = producto.cantidad;                    
+                }
+                return articulo;
+            });
+            // Se asigna el nuevo array a cliente.pedido
+            cliente.pedido = [...pedidoActualizado];
+        } else {
+            // El articulo no existe, lo agregamos al array de pedido
+            cliente.pedido = [...pedido, producto]
+        }
+
+    } else {
+        console.log('No es mayor a 0');
+    }
+
+    console.log(cliente.pedido);
+}
